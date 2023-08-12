b2a25c2d789605c01762ac8172d22d312a345331
Vahid Moradi <vahid.moradi@edreamsodigeo.com>
2023-08-12 11:41:16 +0200

    #4 Added BaseViewModel and the coroutine library

---
+++ releife/src/main/java/app/vahid/android/releife/viewmodel/ReducerContainerHost.kt
@@ -0,0 +1,16 @@
+package app.vahid.android.releife.viewmodel
+
+import app.vahid.android.releife.pattern.Pattern
+import app.vahid.android.releife.pattern.SideEffect
+import app.vahid.android.releife.pattern.State
+import kotlinx.coroutines.flow.Flow
+import org.orbitmvi.orbit.ContainerHost
+
+interface ReducerContainerHost<
+    INTENT : Pattern,
+    STATE : State,
+    SIDE_EFFECT : SideEffect,
+    > : ContainerHost<STATE, SIDE_EFFECT> {
+
+    suspend fun handleIntent(intent: INTENT): Flow<Pattern>
+}
